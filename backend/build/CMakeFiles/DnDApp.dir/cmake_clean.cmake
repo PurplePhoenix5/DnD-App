2ce0c2fa2e48b04b852c87082480670f2b4abf3b
Emanuel <emanuel.charette@ankabut.net>
2025-04-22 08:34:32 +0000

    Update

---
+++ backend/build/CMakeFiles/DnDApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DnDApp.dir/src/main.cpp.o"
+  "CMakeFiles/DnDApp.dir/src/main.cpp.o.d"
+  "DnDApp"
+  "DnDApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DnDApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
