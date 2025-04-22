33eb8e8e2ca58ce948dc412fe9b9bb3b1b79d11a
Emanuel <emanuel.charette@ankabut.net>
2025-04-22 13:38:38 +0000

    Update

--- backend/data/DnDData/dice.js
+++
@@ -1,3 +0,0 @@
-// src/utils/dndData/dice.js
-export const DICE = { d1: 1, d4: 4, d6: 6, d8: 8, d10: 10, d12: 12, d20: 20, };
-export const DIE_LOOKUP = { 1: 'd1', 4: 'd4', 6: 'd6', 8: 'd8', 10: 'd10', 12: 'd12', 20: 'd20', };
